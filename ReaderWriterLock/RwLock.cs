f802acf51a426b7b1e0179d6ce7fdbddc26923af
Maxim <dddg.dddfgg@yandex.ru>
2022-03-30 19:49:30 +0500

    add cw2

---
+++ ReaderWriterLock/RwLock.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading;
+
+namespace ReaderWriterLock
+{
+    public class RwLock : IRwLock
+    {
+        private readonly object lockObject = new();
+        private ulong currentReadersCount;
+        private readonly EventWaitHandle waitHandle = new AutoResetEvent(false);
+        
+        public void ReadLocked(Action action)
+        {
+            lock (lockObject)
+            {
+                Interlocked.Increment(ref currentReadersCount);
+            }
+
+            try
+            {
+                action();
+            }
+            finally
+            {   
+                if (Interlocked.Decrement(ref currentReadersCount) == 0) 
+                    waitHandle.Set();
+            }
+        }
+
+        public void WriteLocked(Action action)
+        {
+            lock (lockObject)
+            {
+                while (Interlocked.Read(ref currentReadersCount) != 0) 
+                    waitHandle.WaitOne();
+                action();
+            }
+        }
+    }
+}
